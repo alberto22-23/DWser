b8cf4f6f6169db6b7e7ce00675dcd764cbea1317
alberto22-23 <jfernandezc22@fpcoruna.afundacion.org>
2022-10-15 19:16:57 +0200

    Ejercicio 2 Introducción a PHP.

---
+++ sprint1apache/www/site3/example2.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <h1>Página de bienvenidas</h1>
+        <?php
+        function dar_bienvenidas($nombre){
+            echo "¡Bienvenido/a, ".$nombre."!";
+        }
+            dar_bienvenidas("Arnold");
+        ?>
+    </body>
+</html>

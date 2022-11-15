26d409d17d8d49bdffa50539108a67227c767e48
alberto22-23 <jfernandezc22@fpcoruna.afundacion.org>
2022-11-15 19:39:06 +0100

    sprint3sesiones-ejercicio7

--- sprint3sesiones/www/mysite/main.php
+++ sprint3sesiones/www/mysite/main.php
@@ -2,13 +2,23 @@
 $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
 ?>
 <html>
+<head>
+<style>
+h2:hover{
+background-color: yellow;
+}
+h2{
+transition: background-color 0.8s linear 0.2s;
+}
+</style>
+</head>
 <body>
 <h2>---Conexión establecida---</h2>
 <h1>Películas</h1>
 <hr>
-<h2>Sesión iniciada por: <?php
+<h3>Sesión iniciada por: <?php
 $nuevoUsuario = $_POST['usuActiv'];
-echo $nuevoUsuario; ?></h2>
+echo $nuevoUsuario; ?></h3>
 <a href="/login.html">Iniciar sesión</a><br>
 <a href="/logout.php">Cerrar sesión</a><br>
 <hr>
@@ -18,17 +28,13 @@ $query = 'SELECT * FROM tPeliculas';
 $result = mysqli_query($db, $query) or die('Query error');
 //Recorrer el resultado
 while ($row = mysqli_fetch_array($result)){
-echo $row['id'];
-echo '<br>';
-echo $row['nombre'];
+echo '<h2>'.$row['id'].' - '.$row['nombre'].'</h2>';
 echo '<br>';
 ?>
 <img src="<?php echo $row['url_imagen'];?>" title="<?php echo $row['nombre']?>">
 <?php
 echo '<br>';
-echo $row['año'];
-echo '<br>';
-echo $row['tema'];
+echo '<h3>'.$row['año'].' - '. $row['tema'].'</h3>';
 echo '<br>';
 }
 mysqli_close($db);
